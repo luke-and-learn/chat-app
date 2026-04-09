67efddc3a7b50816a153dd22c6a02fdf3022ac3e
luke <luke-and-learn@proton.me>
2026-04-09 22:35:38 +0200

    Declared Client type + related functions

--- main.go
+++ main.go
@@ -5,3 +5,35 @@ type Message struct {
 	Type		string	`json:"type"`
 	UserCount	int		`json:"user_count"`
 }
+
+Type Client struct {
+	hub			*Hub
+	conn		*websocket.Conn 
+	send		chan Message
+	username	string
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		var msg Message
+		if err := c.conn.ReadJSON(&msg); err != nil {
+			break
+		}
+		msg.Username = c.username
+		msg.Type = "message"
+		c.hub.broadcast <- msg
+	}
+}
+
+func (c *Client) writePump() {
+	defer c.conn.Close()
+	for msg := range c.send {
+		if err := c.conn.WriteJSON(msg); err != nil {
+			break
+		}
+	}
+}
