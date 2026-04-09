dc6d3466779a6f1568e2c8dd0c8e97a810de26fb
luke <luke-and-learn@proton.me>
2026-04-09 22:53:59 +0200

    Added Hub type + related functions

--- main.go
+++ main.go
@@ -37,3 +37,50 @@ func (c *Client) writePump() {
 		}
 	}
 }
+
+Type Hub struct {
+	clients		map[*Client]bool
+	broadcast	chan Message
+	register	chan *Client
+	unregister	chan *Client
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+			h.broadcastAll(Message{
+				Username:	client.username,
+				Text:		client.username + " joined the chat",
+				Type: 		"join",
+				UserCount:	len(h.clients),
+			})
+		}
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+				h.broadcastAll(Message{
+					username:	client.username,
+					Text: 		client.username + " left the chat",
+					Type: 		"leave",
+					UserCount: 	len(h.clients),
+				})
+			}
+		case msg := <-h.broadcast:
+			h.broadcastAll(msg)
+	}
+}
+
+func (h *Hub) broadcastAll(msg Message) {
+	for client := range h.clients {
+		select {
+		case client.send <- msg:
+		default:
+			close(client.send)
+			delete(h.clients, client)
+		}
+	}
+}
